345f8c892d51843ccb5d5aa41c37cbbc727aff5c
ranaroy-ai <143075343+ranaroy-ai@users.noreply.github.com>
2025-04-07 10:21:40 +0530

    Update score.py

--- deploy/score.py
+++ deploy/score.py
@@ -21,7 +21,25 @@ def init():
     logging.error(f">>>>>>>>>>>>>>>>>>>>>>>>>>> {credential}")
     # Retrieve the secret
     secret = client.get_secret(SECRET_NAME)
+    secret_client_id = client.get_secret("client-id")
+    secret_client_sec = client.get_secret("client-secret")
+    secret_tenant_id = client.get_secret("tenant-id")
+    secret_subscription = client.get_secret("subscription-id")
     secret_value = secret.value
+    secret_client_id_value = secret_client_id.value
+    secret_client_sec_value = secret_client_sec.value
+    secret_tenant_id_value = secret_tenant_id.value
+    secret_subscription_value = secret_subscription.value
+    os.environ["AZURE_TENANT_ID"] = secret_tenant_id_value
+    os.environ["AZURE_CLIENT_ID"] = secret_client_id_value
+    os.environ["AZURE_CLIENT_SECRET"] = secret_client_sec_value
+    os.environ["AZURE_SUBSCRIPTION_ID"] = secret_subscription_value
+    ab = os.getenv(AZURE_TENANT_ID)
+    bc = os.getenv(AZURE_CLIENT_ID)
+    cd= os.getenv(AZURE_CLIENT_SECRET)
+    logging.error(f">>>>>>>>>>>>>>>>>>>>>>>>>>> {ab}")
+    logging.error(f">>>>>>>>>>>>>>>>>>>>>>>>>>> {bc}")
+    logging.error(f">>>>>>>>>>>>>>>>>>>>>>>>>>> {cd}")
     logging.info("Init complete")
     logging.error(f">>>>>>>>>>>>>>>>>>>>>>>>>>> {secret_value}")
     # # AZUREML_MODEL_DIR is an environment variable created during deployment.
